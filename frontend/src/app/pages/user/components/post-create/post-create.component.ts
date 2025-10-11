7e0e5471e3d27a03ab7f0fe87f7c71232ecc7dbe
Daniel Lazar <lazardani0112@gmail.com>
2025-10-11 19:30:48 +0200

    feat(post-create): Add basic form handling.

--- frontend/src/app/pages/user/components/post-create/post-create.component.ts
+++ frontend/src/app/pages/user/components/post-create/post-create.component.ts
@@ -1,13 +1,32 @@
 import { Component } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
 
 import { ButtonModule } from "primeng/button";
 import { CardModule } from 'primeng/card';
 import { TextareaModule } from 'primeng/textarea';
 
 @Component({
+  host: {
+    '(keydown.meta.enter)': 'onSubmit()',
+    '(keydown.control.enter)': 'onSubmit()',
+  },
   selector: 'cubeshares-post-create',
   templateUrl: 'post-create.component.html',
   styleUrl: 'post-create.component.scss',
-  imports: [ButtonModule, CardModule, TextareaModule],
+  imports: [ButtonModule, CardModule, ReactiveFormsModule, TextareaModule],
 })
-export class PostCreateComponent { }
+export class PostCreateComponent {
+  protected form = this.fb.group({
+    description: this.fb.control(''),
+  });
+
+  constructor(private readonly fb: FormBuilder) { }
+
+  protected onSubmit(): void {
+    console.log(this.form.getRawValue());
+  }
+
+  protected onClear(): void {
+    this.form.reset();
+  }
+}
